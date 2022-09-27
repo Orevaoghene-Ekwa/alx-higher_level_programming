a0047fabbaa62a4c9ccc73c6c730cb69bb5f2f1a
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2022-09-27 04:28:36 -0700

    removed unecessary characters

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,9 +5,9 @@ num_str = repr(number)
 last_digit_str = num_str[-1]
 last_digit = int(last_digit_str)
 if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5\n")
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0\n")
+    print(f"Last digit of {number} is {last_digit} and is 0")
 elif last_digit < 6 and not 0:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0\n")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
 
