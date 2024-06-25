4288fa0e6570b62851718334522349cb02847ac2
Orevaoghene Ekwa <ekwaorevaoghene@gmailcom>
2024-06-25 01:32:07 -0700

    fixed run on import

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # 5-variable_load.py
+from variable_load_5 import a
 
 if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
+    exit()
 
-    print(a)
+print("{:d}".format(a))
