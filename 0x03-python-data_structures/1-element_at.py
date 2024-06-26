a9e210d51b41ef150251c60f40c51798b6a4a989
Orevaoghene Ekwa <ekwaorevaoghene@gmailcom>
2024-06-26 10:57:30 -0700

    white space

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def element_at(my_list, idx):
     """
     Returns an element from a list at given index
@@ -7,4 +8,5 @@ def element_at(my_list, idx):
     list_len = len(my_list)
     if idx < 0 or idx >= list_len:
         return
+
     return(my_list[idx])
