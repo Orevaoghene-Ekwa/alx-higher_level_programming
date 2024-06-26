c25bdb3962d6559f491b6aee439b6bc201e4976f
Orevaoghene Ekwa <ekwaorevaoghene@gmailcom>
2024-06-26 10:52:03 -0700

    Retrieve element from a list at index

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """
+    Returns an element from a list at given index
+    Returns none if not found
+    """
+    list_len = len(my_list)
+    if idx < 0 or idx >= list_len:
+        return
+    return(my_list[idx])
