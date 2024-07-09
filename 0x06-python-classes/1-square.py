cdc98dead13d92aca22b9fd713e59b39d8a9e450
Orevaoghene <ekwaorevaoghene@gmail.com>
2024-07-09 02:18:08 -0700

    fixed documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
-class Square:
-        """Simple square class with its size as a field"""
+"""Defines a class Square."""
 
+class Square:
+    """
+    Simple square class with its size as a field
+
+    Attributes:
+        size: size of  a square (1 side)
+    """
     def __init__(self, size):
         """ Instance the class Square
-            Arguments:
-                @size: the size of every side of the Square"""
+        
+        Arguments:
+            size: the size of every side of the Square
+        """
         self.__size = size
